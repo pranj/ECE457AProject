3f8abeaccd687e08da5cda6718f275debe81b832
Ryan Zhang <xy22zhan@uwaterloo.ca>
2014-07-25 00:47:16 -0400

    Cleaning

--- calculate_cost.m
+++ calculate_cost.m
@@ -1,21 +1,20 @@
 % This function outputs the cost of each receiver and the total cost
 
 function [totalCost, cost] = calculate_cost(solution, costs, depotCosts)
-[num_receivers, num_points] = size(solution);
-cost = zeros(1, num_receivers);
+[numReceivers, numPoints] = size(solution);
+cost = zeros(1, numReceivers);
 
-for receiver = 1:num_receivers
+for receiver = 1:numReceivers
     % Calculate cost of a single tour 
     % Find sum of cost of adjacent 
-    for i = 1:num_points
+    for i = 1:numPoints
         if solution(receiver, i + 1) == 0
             break
         else
             from = solution(receiver, i);
             to = solution(receiver, i + 1);
             if i == 1
-                cost(receiver) = cost(receiver) ...
-                    + depotCosts(from);
+                cost(receiver) = depotCosts(from);
             end
             
             cost(receiver) = cost(receiver) + costs(from, to);

4677a5783f48b7ac432a45de3b087decd5d38b91
Sola Ogunsakin <juniorsola@yahoo.com>
2014-07-25 11:01:11 -0400

    fix cost function

--- calculate_cost.m
+++ calculate_cost.m
@@ -2,19 +2,16 @@
 
 function [totalCost] = calculate_cost(solution, costs, depotCosts)
 
-currentCost = 0;
 totalCost = 0;
 
 for i = 1:size(solution, 2)
     if solution(i) == 0
-        totalCost = totalCost + currentCost;
-        currentCost = 0;
-    elseif i == 1 || solution(i-1) == 0;
-        currentCost = currentCost + depotCosts(solution(i));
+        continue
+    elseif i == 1 || solution(i - 1) == 0;
+        totalCost = totalCost + depotCosts(solution(i));
     else
-        from = solution(i-1);
+        from = solution(i - 1);
         to = solution(i);
-        currentCost = currentCost + costs(from, to);
+        totalCost = totalCost + costs(from, to);
     end
-    
 end

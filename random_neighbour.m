81c04d66b66b1cf9e41f8a1a742b03eb84811015
Sola Ogunsakin <juniorsola@yahoo.com>
2014-07-25 11:29:04 -0400

    random neighbour function

---
+++ random_neighbour.m
@@ -0,0 +1,13 @@
+% This function generates a random neighbour from the current solution.
+% There is a 50% chance that the neighbour is generated with a swap operation
+% and a 50% chance that the neighbour is generated an add/remove operation
+
+function [neighbour] = random_neighbour(solution)
+
+r = rand(1, 1);
+
+if r < 0.5
+    neighbour = random_swap(solution)
+else
+    neighbour = random_add_remove(solution)
+end

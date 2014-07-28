e04d8f7f449fb7bacc25dd99e68db663b4ea25d1
Ryan Zhang <xy22zhan@uwaterloo.ca>
2014-07-27 20:12:20 -0400

    Initial tabu search implementation

--- ts/get_neighbours.m
+++ ts/get_neighbours.m
@@ -1,24 +1,29 @@
-function [neighbours] = get_neighbours(solution)
+function [neighbours, method] = get_neighbours(solution)
 numRcvr = sum(solution == 0) + 1;
 
 numPoints = size(solution, 2) - (numRcvr - 1);
 numNeighbours = nchoosek(numPoints, 2) + numPoints*(numPoints + numRcvr - 2);
-neighbours = zeros(numNeighbours, size(solution, 2));
+neighbourhood = zeros(numNeighbours, size(solution, 2) + 3);
 
 neighbourIdx = 1;
 for i = 1:numPoints
     for j = i+1:numPoints
-        neighbours(neighbourIdx, :) = swap(solution, i, j);
+        neighbourhood(neighbourIdx, 1:end-3) = swap(solution, i, j);
+        neighbourhood(neighbourIdx, end-2:end) = [0 i j];
         neighbourIdx = neighbourIdx + 1;
     end
 end
 
 for i = 1:numPoints
     for j = 1:numPoints
-        neighbours(neighbourIdx, :) = add_remove(solution, i, j);
+        neighbourhood(neighbourIdx, 1:end-3) = add_remove(solution, i, j);
+        neighbourhood(neighbourIdx, end-2:end) = [1 i j];
         neighbourIdx = neighbourIdx + 1;
     end
 end
 
-neighbours = unique(neighbours, 'rows');
+neighbourhood = unique(neighbourhood, 'rows');
+neighbours = neighbourhood(:, 1:end-3);
+method = neighbourhood(:, end-2:end);
+
 end
---
+++ ts/tabu_search.m
@@ -0,0 +1,69 @@
+function [globalBestSol, globalBestCost] = tabu_search( ...
+            costs, tabuLength, numIterations, numRcvr, initialSol)
+numPoints = size(initialSol, 2) - numRcvr + 1;
+swap_tabu = zeros(numPoints, numPoints);
+insert_tabu = zeros(numPoints + numRcvr - 1, numPoints + numRcvr - 1);
+
+globalBestSol = initialSol;
+globalBestCost = calculate_cost(initialSol, costs, costs(end, :));
+        
+for n = 1:numIterations
+    % Get the best solution in the neighbourhood of the current solution
+    % Avoid Tabu moves, consider aspiration criteria
+    [neighbours method] = get_neighbours(initialSol);
+    bestNeighbourCost = Inf;
+    bestNeighbourIdx = 1;
+    for i = 1:size(neighbours, 1)
+        neighbourCost = calculate_cost(neighbours(i), costs, costs(end, :));
+        if method(i, 1) == 0
+            % check swap tabu
+            point1 = method(i, 2);
+            point2 = method(i, 3);
+            if swap_tabu(point1, point2) > 0
+                if neighbourCost < globalBestCost
+                    bestNeighbourCost = neighbourCost;
+                    bestNeighbour = neighbours(i);
+                else
+                    continue
+                end
+            end
+            
+        elseif method(i, 1) == 1
+            % check insert tabu
+            removePoint = method(i, 2);
+            insertIdx = method(i, 3);
+            if insert_tabu(removePoint, insertIdx) > 0
+                if neighbourCost < globalBestCost
+                    bestNeighbourCost = neighbourCost;
+                    bestNeighbour = neighbours(i);
+                else
+                    continue
+                end
+            end
+        end
+         
+        if neighbourCost < bestNeighbourCost
+            bestNeighbourCost = neighbourCost;
+            bestNeighbour = neighbours(i);
+            bestNeighbourIdx = i;
+        end
+    end
+    
+    % tabu best neighbour method
+    if method(bestNeighbourIdx, 1) == 0
+        point1 = method(bestNeighbourIdx, 2);
+        point2 = method(bestNeighbourIdx, 3);
+        swap_tabu(point1, point2) = tabuLength;
+    elseif method(bestNeighbourIdx, 2) == 0
+        removePoint = method(bestNeighbourIdx, 2);
+        insertIdx = method(bestNeighbourIdx, 3);
+        insert_tabu(removePoint, insertIdx) = tabuLength;
+    end
+    
+    if bestNeighbourCost < globalBestCost
+        globalBestCost = bestNeighbourCost;
+        globalBestSol = bestNeighbour;
+    end
+end
+        
+end

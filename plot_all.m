f7b63e33de82832d70ad464e8b3b5b931a8bf88c
theProf <theprof@gerggle.com>
2014-07-29 06:01:22 -0400

    plotting

--- plot_all.m
+++ plot_all.m
@@ -3,13 +3,15 @@ phandle = [];
 idx = 1;
 out = [];
 
-[~,best, out] = ga(costs, 100, 20, .9, .1, 51, 2)
-[~,best2, out2] = ga(costs, 100, 20, .87, .91, 51, 2)
-phandle(1) = plot(out, colours(1), 'LineWidth', 2)
+[~,best2, out(1)] = ga(costs, 100, 20, .91, .91, 51, 2);
+[~, l, out(2)] = ant_colony_optimization(Costs, 50, 51, 5, 12, 0.01, 1, 1, 0.15, 0.8850);
+[~, l, out(3)] = simulated_annealing(costs, bSol, 0.9, 0.8, 10E-10, 100);
+phandle(1) = plot(out(1), colours(1), 'LineWidth', 2);
 hold on
-phandle(2) = plot(out2, colours(2), 'LineWidth', 2)
-title('Shit be cray')
+phandle(2) = plot(out(2), colours(2), 'LineWidth', 2);
+phandle(3) = plot(out(3), colours(3), 'LineWidth', 2);
+title('Cost Analysis')
 xlabel('iteration')
 ylabel('cost')
-legend(phandle, 'ga-1', 'ga-2')
+legend(phandle, 'ga', 'aco', 'sa', 'ts')
 hold off

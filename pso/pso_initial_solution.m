9ae0048865fd51173499509014b0724a249250dc
Ryan Zhang <xy22zhan@uwaterloo.ca>
2014-07-29 07:37:25 -0400

    Fix to accomodate intersect change between 2012 and 2013

--- pso/pso_initial_solution.m
+++ pso/pso_initial_solution.m
@@ -4,6 +4,9 @@ solutionSize = numPoints + numReceivers - 1;
 solution = randperm(solutionSize);
 dividerValues = (solutionSize - numReceivers + 2:solutionSize);
 [~, dividers] = intersect(solution, dividerValues);
+if size(dividers, 2) == 1
+    dividers = dividers';
+end
 for x = 1:size(dividers, 2)
     solution(dividers(x)) = -x;
 end

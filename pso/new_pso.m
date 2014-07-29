07359200f010e0153a99c8e814576cf6c459b733
Ryan Zhang <xy22zhan@uwaterloo.ca>
2014-07-29 06:40:20 -0400

    Add initial velocity to PSO

--- pso/new_pso.m
+++ pso/new_pso.m
@@ -1,10 +1,9 @@
 function [GlobalBest, GlobalBestCost] = new_pso( ...
                 Costs, NumIterations, NumParticles, ...
                 NumPoints, NumReceivers, ...
-                inertiaWeight, cognitiveAccel, socialAccel)
+                inertiaWeight, cognitiveAccel, socialAccel, maxVelocity)
     SolutionSize = NumPoints + NumReceivers - 1;
-    maxVelocity = 50;
-
+    
     % initialize particle position/solution
     ParticlePosition = zeros(NumParticles, SolutionSize);
     ParticleBest = zeros(NumParticles, SolutionSize);
@@ -12,18 +11,37 @@ function [GlobalBest, GlobalBestCost] = new_pso( ...
     
     % 3D matrix of velocity swap sets
     ParticleVelocity = zeros(NumParticles, 2, maxVelocity);
+    
     for particle = 1:NumParticles
         ParticleSolution = pso_initial_solution(NumPoints, NumReceivers);
         
         ParticlePosition(particle, :) = ParticleSolution;
         ParticleBest(particle, :) = ParticlePosition(particle, :); 
         ParticleBestCost(particle) = calculate_cost(ParticlePosition(particle, :), Costs, Costs(end, :));
+        
+        % generate initial random velocity for each particle
+        velocitySize = randi(maxVelocity);
+        
+        for y = 1:velocitySize
+            % get two random points in solution
+            firstIndex = randi(size(ParticleSolution, 2));
+            secondIndex = randi(size(ParticleSolution, 2));
+            while firstIndex == secondIndex
+                firstIndex = randi(size(ParticleSolution, 2));
+                secondIndex = randi(size(ParticleSolution, 2));
+            end
+
+            ParticleVelocity(particle, :, y) = [ParticleSolution(firstIndex) ParticleSolution(secondIndex)];
+        end
     end
     
     % initialize global best
     [GlobalBestCost GlobalBestParticle] = min(ParticleBestCost);
     GlobalBest = ParticleBest(GlobalBestParticle, :);
-
+    
+    % TEMP
+    %plot_ponts = zeros(50, 1);
+    
     % perform PSO
     for iteration = 1:NumIterations
         for particle = 1:NumParticles
@@ -50,7 +68,7 @@ function [GlobalBest, GlobalBestCost] = new_pso( ...
             cogCount = floor(cogCoeff * size(vCognitive, 1));
             socCount = floor(socCoeff * size(vSocial, 1));
             
-            nextVelocity = calculate_next_velocity(inertiaCount, vInertia, cogCount, vCognitive, socCount, vSocial);
+            nextVelocity = calculate_next_velocity(inertiaCount, vInertia, cogCount, vCognitive, socCount, vSocial, maxVelocity);
             
             % update particle position
             ParticlePosition(particle, :) = update_position(ParticlePosition(particle, :), nextVelocity);
@@ -68,11 +86,17 @@ function [GlobalBest, GlobalBestCost] = new_pso( ...
                 GlobalBest = ParticlePosition(particle, :);
             end
             
+            % zero out previous particle velocity
             % loop through to update particle velocity for next iteration
+            ParticleVelocity(particle, :, :) = zeros(1, 2, maxVelocity);
             for y = 1:size(nextVelocity, 1)
                 ParticleVelocity(particle, :, y) = nextVelocity(y, :);
             end
-        end   
+        end
+        
+        %plot_points(iteration) = min(ParticleBestCost);
+        %plot(plot_points)
+        %drawnow
     end
 end
 
@@ -85,27 +109,27 @@ function [position] = update_position(position, velocity)
 end
 
 function [velocity] = calculate_velocity(best, current)
-if ~isequal(current, best)
-    velocity = zeros(size(best, 2), 2) - 1;
-    for x = 1:size(best, 2)
-        if best(x) ~= current(x)
-            % account for trivial swaps
-            % do not want to swap 0, 3 then 3, 0
-            if find(velocity(:, 2) == current(x))
-                continue
-            else
-                velocity(x, :) = [current(x) best(x)];
+    if ~isequal(current, best)
+        velocity = zeros(size(best, 2), 2) - 1;
+        for x = 1:size(best, 2)
+            if best(x) ~= current(x)
+                % account for trivial swaps
+                % do not want to swap 0, 3 then 3, 0
+                if find(velocity(:, 2) == current(x))
+                    continue
+                else
+                    velocity(x, :) = [current(x) best(x)];
+                end
             end
+
         end
-        
+        velocity(velocity(:, 2) < 0, :) = [];
+    else
+        velocity = [];
     end
-    velocity(velocity(:, 2) < 0, :) = [];
-else
-    velocity = [];
-end
 end
 
-function [nextVelocity] = calculate_next_velocity(w, vInertia, c1r1, vCog, c2r2, vSoc)
+function [nextVelocity] = calculate_next_velocity(w, vInertia, c1r1, vCog, c2r2, vSoc, maxV)
     if any([w c1r1 c2r2])
         velocityIndex = 1;
         nextVelocity = zeros(w + c1r1 + c2r2, 2);
@@ -144,5 +168,12 @@ function [nextVelocity] = calculate_next_velocity(w, vInertia, c1r1, vCog, c2r2,
         end
     else
         nextVelocity = [];
+    end
+    
+    % limit number of swaps in velocity by the maxV
+    if size(nextVelocity, 1) > maxV
+        n = size(nextVelocity, 1);
+        idx = randperm(n, maxV);
+        nextVelocity = nextVelocity(idx, :);
     end
 end

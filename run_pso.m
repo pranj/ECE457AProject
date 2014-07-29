6ba70a6c0071e1ac716cce6711dac8fd6cf6b523
Pranjal Singh <pranjals30@gmail.com>
2014-07-29 06:49:07 -0400

    run pso

--- run_pso.m
+++ run_pso.m
@@ -1,20 +1,22 @@
 function [costs] = run_pso(CostData)
-    costs = zeros(5, 3);
+    costs = zeros(5, 4);
     
-    InertiaWeight = 0.5
-    CognitiveAccel = 0.5
-    NumParticle = 100
-    SocialAccel = 0.5
+
+    InertiaWeight = 0.5;
+    CognitiveAccel = 0.5;
+    SocialAccel = 0.5;
+    MaxVelocity = 10;
     
-    NumIterations = 50;
+    NumParticle = 100;
+    NumIterations = 100;
     NumReceivers = 5;
     NumPoints = 51;
     
-    params = [InertiaWeight CognitiveAccel SocialAccel];
-    change = [0.25 0.25 0.25];
+    params = [MaxVelocity InertiaWeight CognitiveAccel SocialAccel];
+    change = [8 0.1 0.2 0.2];
     
     NumOfRuns = 4;
-    for i = 1:3
+    for i = 1:4
         for k = 1:5
             total = 0;
             cost = 0;
@@ -22,7 +24,7 @@ function [costs] = run_pso(CostData)
                 [~, cost] = ...
                     new_pso(CostData, NumIterations, NumParticle, ...
                     NumPoints, NumReceivers, InertiaWeight, ...
-                    CognitiveAccel, SocialAccel);
+                    CognitiveAccel, SocialAccel, MaxVelocity);
                 total = total + cost;
             end
             costs(k, i) = total/NumOfRuns;
